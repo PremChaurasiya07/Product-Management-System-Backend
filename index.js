5c4dc02fef300e43a3ecc38714f41e6cac37220b
PremChaurasiya07 <premchaurasiya2006@gmail.com>
2025-01-01 16:19:11 +0530

    error solved

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const DBURL=process.env.DB_URL;
 // app.use(bodyParser.json());
 
 app.use(cors(
-    { origin: 'http://localhost:5173', }
+    { origin: 'https://product-management-system-frontend-pied.vercel.app/', }
 ));
 app.use(express.json());
 
